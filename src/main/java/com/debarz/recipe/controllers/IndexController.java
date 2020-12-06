8d27d30a68bce4fd7bc288cea1a0284c0a405df4
deividasbardzenis <deividasbarzdenis@yahoo.com>
2020-12-06 17:52:51 +0200

    Added Ingredient, Notes, UnitOfMeasure POJO. All so added entity annotations @OneToOne and @ManyToOne

---
+++ src/main/java/com/debarz/recipe/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.debarz.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public  String getIndexPage(){
+        System.out.println("1234---------------------------------------------------");
+        return "index";
+    }
+}
